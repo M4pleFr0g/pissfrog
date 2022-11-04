b2971c82bfb4d470d90a7f269449e50f2c8acd47
alexei <agardea25@petk12.org>
2022-11-04 13:10:42 -0700

    started gui

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import PySimpleGUI as sg
+
+def start_gui():
+    sg.Window(title="Hello World", layout=[[]], margins=(200, 200)).read()
+start_gui()
